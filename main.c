9a94bd6c5501ad8deeae24cea30568b6f4837298
rasteri <rasteri@gmail.com>
2022-12-11 22:47:38 +0000

    re enabled menu

--- main.c
+++ main.c
@@ -157,116 +157,6 @@ void InitPWM3(UINT8 polar)
 //PWM1, PWM2, PWM3_
 void main()
 {
-	//PORT_CFG &= ~bP2_OC;
-	/*P2_DIR |= bPWM1 | bPWM2; //It is recommended to set the pin to push-pull output when turning on PWM
-
-	SetPWMClk(12); //Set the clock division factor of PWM1&2 to 12
-	InitPWM1(1);   //PWM1 initialization, active low
-	InitPWM2(1);   //PWM2 initialization, active high
-	InitPWM3(1);
-	SetPWMCycle(0xff);
-	SetPWM1Dat(0x00);
-	SetPWM2Dat(0x00);
-
-	P0 = 0xFF;
-	P0_PU = 0x00;
-	PORT_CFG |= bP0_DRV;
-
-	P2 = 0xFF;
-	P2_PU = 0x00;
-	PORT_CFG |= bP2_DRV;
-
-	P4_DIR = 0xFF;
-	P4_OUT = 0xFF;
-	P4_PU = 0x00;
-	P4_CFG |= bP4_DRV;
-
-
-
-
-	T3_CK_SE_L = 0x20;
-	T3_CK_SE_H = 0;
-	T3_END_H = 0;
-	T3_END_L = 255;
-	T3_FIFO_L = 0;
-	T3_FIFO_H = 0;
-	uint8_t bright = 0;
-	uint8_t seq = 0, r = 0, g = 0, b = 0, nothere = 0;*/
-
-	/*
-			while (bright < 254)
-		{
-			bright++;
-			//SetPWM1Dat(bright);
-			T3_FIFO_L = bright;
-			T3_FIFO_H = 0;
-			//T3_FIFO_L = (uint8_t)(((uint16_t)255-bright * (uint16_t)255-bright) >> 8);
-			//SetPWM2Dat((uint8_t)(((uint16_t)255-bright * (uint16_t)255-bright) >> 8));
-			delayUs(2000);
-		};
-		while (bright > 1)
-		{
-			bright--;
-			//SetPWM1Dat(bright);
-			T3_FIFO_L = bright;
-			T3_FIFO_H = 0;
-			//T3_FIFO_L = (uint8_t)(((uint16_t)255-bright * (uint16_t)255-bright) >> 8);
-			//SetPWM1Dat((uint8_t)(((uint16_t)bright * (uint16_t)bright) >> 8));
-
-			//SetPWM2Dat((uint8_t)(((uint16_t)255-bright * (uint16_t)255-bright) >> 8));
-			delayUs(2000);
-		};
-	*/
-	/*	while (1)
-	{
-		nothere = 0;
-		if (fadeseq[seq].r > r)
-		{
-			r++;
-			nothere = 1;
-		}
-		else if (fadeseq[seq].r < r)
-		{
-			r--;
-			nothere = 1;
-		}
-
-		if (fadeseq[seq].g > g)
-		{
-			g++;
-			nothere = 1;
-		}
-		else if (fadeseq[seq].g < g)
-		{
-			g--;
-			nothere = 1;
-		}
-
-		if (fadeseq[seq].b > b)
-		{
-			b++;
-			nothere = 1;
-		}
-		else if (fadeseq[seq].b < b)
-		{
-			b--;
-			nothere = 1;
-		}
-
-		SetPWM1Dat((uint8_t)(((uint16_t)r * (uint16_t)r) >> 10));
-		SetPWM2Dat((uint8_t)(((uint16_t)g * (uint16_t)g) >> 10));
-		T3_FIFO_L = (uint8_t)(((uint16_t)b * (uint16_t)b) >> 8);
-		T3_FIFO_H = 0;
-
-		if (!nothere)
-			seq++;
-
-		if (seq == 7)
-			seq = 0;
-
-		delayUs(1000);
-	}
-	*/
 
 	InitSystem();
 
@@ -284,12 +174,9 @@ void main()
 
 	//port4 setup
 	P4_DIR = 0b00010100; //4.0 is RXD, 4.2 is Blue LED, 4.3 is MOUSE DATA (actually input, since we're faking open drain), 4.4 is TXD, 4.6 is SWITCH
-	P4_PU = 0x00;		 // no pullups
+	P4_PU =  0b01000000;		 // pullup on switch
 	P4_OUT = 0b00000100; //LEDs off (i.e. HIGH), MOUSE DATA low (since it's switched by toggling input on and off, i.e. faking open drain)
 
-	// Switch on LEDs
-	/*P2 &= ~0b00110000;
-	P4_OUT &= ~0b00000100;*/
 
 	// timer0 setup
 	TMOD = (TMOD & 0xf0) | 0x02; // mode 1 (8bit auto reload)
@@ -299,27 +186,9 @@ void main()
 	ET0 = 1; //enable timer0 interrupt;
 	EA = 1;	 // enable all interrupts
 
-	/*while (1)
-	{
-
-		if (GetPort(PORT_MOUSE, DATA)){
-			//if (P4_IN &= 0b00001000){
-			P2 &= ~0b00110000;
-			P4_OUT &= ~0b00000100;
-		}
-		else {
-			P2 |= 0b00110000;
-			P4_OUT |= 0b00000100;
-		}
-	}*/
 
 	printf("Ready\n");
 
-	/*OutPort(PORT_KEY, DATA, 1);
-	OutPort(PORT_KEY, CLOCK, 1);
-
-	OutPort(PORT_MOUSE, DATA, 1);
-	OutPort(PORT_MOUSE, CLOCK, 1);*/
 
 	memset(SendBuffer, 0, 255);
 
@@ -327,32 +196,14 @@ void main()
 
 	while (1)
 	{
-		/*if (!(P4_IN & (1 << 6)))
+		if (!(P4_IN & (1 << 6)))
 			MenuActive = 1;
 
 		if (MenuActive)
-			Menu_Task();*/
+			Menu_Task();
 
 		ProcessUsbHostPort();
 		ProcessKeyboardLed();
 		HandleRepeats();
 	}
 }
-
-/*int main()
-{
-	InitSystem();
-
-	while (TRUE)
-	{
-
-		//printf("heh\n");
-		ProcessUsbHostPort();
-
-		ProcessKeyboardLed();
-	}
-	
-	return 0;
-}
-
-*/

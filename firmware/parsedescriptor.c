6e80bc7d21f25e5737f89ae25442e5e4a45a69f3
rasteri <rasteri@gmail.com>
2024-07-11 19:00:10 +0100

    f

--- firmware/parsedescriptor.c
+++ firmware/parsedescriptor.c
@@ -158,7 +158,7 @@ static uint8_t *FetchItem(uint8_t *start, uint8_t *end, HID_ITEM *item)
 
 }
 
-void CreateSeg()                                                                                              \
+#define CreateSeg()                                                                                              \
 	{                                                                                                            \
 		if (pHidSegStruct->reports[hidGlobalPnt->reportID]->firstHidSeg == NULL)                                 \
 		{                                                                                                        \

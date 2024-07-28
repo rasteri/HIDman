e86571d79424fcd0b1d3bc8780a77e66ffa695c1
rasteri <rasteri@gmail.com>
2024-07-28 21:37:38 +0100

    updated test harness

--- firmware/system.c
+++ firmware/system.c
@@ -114,53 +114,8 @@ void	mDelaymS( UINT16 n )                                                  // ï¿
 }
 
 
-#define testintsize(TP, SZ) \
-	if (sizeof(TP) != SZ) printf("type " #TP " is not " #SZ " bytes! (%d instead)\n", sizeof(TP));
 
 
-/*
-	Func to test the sizes of various types
-	Mainly just to remind me how big all the various weird pointers are
-*/
-/*void testintsizes(){
-
-	// trad C - note ints and shorts are both 2 bytes
-	testintsize(char, 1); testintsize(unsigned char, 1);
-	testintsize(short, 2); testintsize(unsigned short, 2);
-	testintsize(int, 2); testintsize(unsigned int, 2);
-	testintsize(long, 4); testintsize(unsigned long, 4);
-	testintsize(long long, 8); testintsize(unsigned long long, 8);
-
-	// C99, this should be obvious but can't hurt to test
-	testintsize(int8_t, 1); testintsize(uint8_t, 1);
-	testintsize(int16_t, 2); testintsize(uint16_t, 2);
-	testintsize(int32_t, 4); testintsize(uint32_t, 4);
-	testintsize(int64_t, 8); testintsize(uint64_t, 8);
-
-	// size_t is 2 bytes
-	testintsize(size_t, 2);
-
-	// stuff in xdata should work the same
-	testintsize(__xdata uint8_t, 1); testintsize(__xdata uint32_t, 4);
-
-	// generic pointers should be 24bit
-	testintsize(uint8_t *, 3); testintsize(uint32_t *, 3);
-
-	// code pointers should be 16bit
-	testintsize(__code uint8_t *, 2); testintsize(__code uint32_t *, 2);
-
-	// data pointers should be 8bit
-	testintsize(__data uint8_t *, 1); testintsize(__data uint32_t *, 1);
-
-	// idata pointers should be 8bit
-	testintsize(__idata uint8_t *, 1); testintsize(__idata uint32_t *, 1);
-
-	// xdata pointers should be 16bit
-	testintsize(__xdata uint8_t *, 2); testintsize(__xdata uint32_t *, 2);
-
-	// pdata pointers should be 8bit (they are paged 256-byte segments of xdata), page is selected by P2 (or an SFR? depends on 8051 variant)
-	testintsize(__pdata uint8_t *, 1); testintsize(__pdata uint32_t *, 1);
-}*/
 
 void GPIOInit(void){
 	

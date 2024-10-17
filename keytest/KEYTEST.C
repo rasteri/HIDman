f5ea4f1caedc98c66cdcd23350b15a17a806b3a0
rasteri <rasteri@gmail.com>
2024-10-17 21:40:42 +0100

    Keyboard tester work

--- keytest/KEYTEST.C
+++ keytest/KEYTEST.C
@@ -31,20 +31,158 @@ Options :
 
 */
 
+char ScreenBuf[0x781];
+    
 
-KeyDef * FindXTKey(unsigned char *Scancode) {
+#define TYPE_MAKE 0
+#define TYPE_BREAK 1
+
+KeyDef *PrevHighlightKey;
+unsigned char PrevOutType;
+unsigned int NumDupHighlights;
+
+void HighlightKey(KeyDef *Key, unsigned char outtype)
+{
+    char x, y;
+
+    gotoxy(14, 24);
+    printf("                                                  ");
+
+    gotoxy(14, 24);
+    if (outtype == TYPE_MAKE){
+        printf("Pressed %s", Key->Name);
+        textcolor(WHITE);
+        textbackground(LIGHTGRAY);
+    }
+    else if (outtype == TYPE_BREAK){
+        printf("Released %s", Key->Name);
+        textcolor(BLACK);
+        textbackground(LIGHTGRAY);
+    }
+
+    for (x = Key->XPos; x < Key->XPos + Key->Xsize; x++) {
+	    for (y = Key->YPos; y < Key->YPos + Key->Ysize; y++) {
+            gotoxy(x+1, y+1);
+            putch(ScreenBuf[(y*80) + x]);
+        }
+    }
+
+
 
 }
 
+int CompareScanCode(unsigned char *array, unsigned char *s2, unsigned int length) {
+    int index = 0;
+
+    // first compare length (first byte of Array)
+    if (length != array[0]) 
+        return 0;
+
+    //now compare actual codes
+    for (index = 0; index < length; index++) {
+        gotoxy(1, 1);
+        printf("%d %d", length);
+        if (array[index + 1] != s2[index])
+            return 0;
+    }
+    return 1;
+}
+
+
+
+KeyDef * FindXTKey(unsigned char *OutType, unsigned char *Scancode, unsigned int length) {
+    unsigned int cunt = 0;
+    unsigned int index = 0;
+
+    KeyDef *currkeydef;
+
+    for (cunt = 0; cunt < 105; cunt++) {
+        currkeydef = &KeyDefs[cunt];
+
+        if (CompareScanCode(currkeydef->XTMake, Scancode, length)){
+            *OutType = TYPE_MAKE;
+            return currkeydef;
+        }
+        if (CompareScanCode(currkeydef->XTBreak, Scancode, length)){
+            *OutType = TYPE_BREAK;
+            return currkeydef;
+        }
+    }
+    return NULL;
+}
+
+volatile unsigned char scanbufindex = 0;
+volatile unsigned char scancodebuf[16];
+
 static void interrupt keyb_int()
 {
+    unsigned char code;
+    unsigned char cunt;
+
+    unsigned char outtype;
+
+    KeyDef *foundkey;
+
+    scancodebuf[scanbufindex++] = inp(0x60);
+    if (scanbufindex >= 16)
+        scanbufindex = 0;
+
+    // single E0 scancode ignore
+    if (scanbufindex == 1 && scancodebuf[0] == 0xE0) goto endage;
+
+    foundkey = FindXTKey(&outtype, scancodebuf, scanbufindex);
+    if (foundkey == NULL) {
+        gotoxy(14, 23);
+        printf("Unknown Scancode :                                ");
+        gotoxy(34, 23);
+        for (cunt = 0; cunt < scanbufindex; cunt++)
+            printf("%02x", scancodebuf[cunt]);
+    }
+    else {
+
+        // always clear count
+        gotoxy(14, 25);
+        printf("       ");
+
+            gotoxy(14, 23);
+            printf("        Scancode :                                ");
+            gotoxy(34, 23);
+            for (cunt = 0; cunt < scanbufindex; cunt++)
+            printf("%02x", scancodebuf[cunt]);
+
+        // if same key as last time, just increment counter and display on screen
+        if (foundkey == PrevHighlightKey && outtype == PrevOutType){
+            gotoxy(14, 25);
+            printf("(x%u)", NumDupHighlights++);
+        }
+        else {
+
+            NumDupHighlights = 0;
+            HighlightKey(foundkey, outtype);
+            
+            // special case for 2B, also light up Euro1 key (since keycodes are the same)
+            if (foundkey->XTMake[1] == 0x2B)
+                HighlightKey(&KEYDEF_EURO1, outtype);
+        }
+        
+        PrevHighlightKey = foundkey;
+        PrevOutType = outtype;
+
+        scanbufindex = 0;
+
+    }
+
+    endage:
+
+    outp(0x20, 0x20); 
+
     /*
     static unsigned char buffer;
     unsigned char rawcode;
     unsigned char make_break;
     int scancode;
 
-    rawcode = inp(0x60);            // read scancode from keyboard controller 
+
     make_break = !(rawcode & 0x80); // bit 7: 0 = make, 1 = break 
     scancode = rawcode & 0x7F;
 
@@ -97,25 +235,11 @@ int ctrlbrk_handler(void)
     return 0;
 }
 
-char ScreenBuf[0x781];
-    
-void HighlightKey(KeyDef *Key)
-{
-    char x, y;
 
-    textcolor(BLACK);
-    textbackground(WHITE);
-
-    for (x = Key->XPos; x < Key->XPos + Key->Xsize; x++) {
-	    for (y = Key->YPos; y < Key->YPos + Key->Ysize; y++) {
-            gotoxy(x+1, y+1);
-            putch(ScreenBuf[(y*80) + x]);
-        }
-    }
-}
 
 int main()
 {
+    unsigned char oldscanbufindex = 0;
     unsigned char cunt = 0;
     FILE *keyfile;
     ctrlbrk(ctrlbrk_handler);
@@ -133,11 +257,18 @@ int main()
 
     printf("%s", ScreenBuf);
 
-    for (cunt = 0; cunt < 40; cunt++){
+    /*for (cunt = 0; cunt < 40; cunt++){
         HighlightKey(&KeyDefs[cunt]);
+        delay(200);
+    }*/
+    oldscanbufindex = scanbufindex;
+    while(1) {
+        scanbufindex = oldscanbufindex;
+        delay(100);
+        if (scanbufindex == oldscanbufindex)
+            scanbufindex = 0;
+        delay(100);
     }
-
-goto ehoh;
 
     /*while (!normal_keys[1])
     {
@@ -149,7 +280,7 @@ goto ehoh;
 
     ehoh:
 
-    //gotoxy(1, 6);
+    gotoxy(1, 6);
     _setcursortype(_NORMALCURSOR);
     unhook_keyb_int();
     return 0;
